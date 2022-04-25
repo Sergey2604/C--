eedf3178be3f3444db2af7bcad409d828ca474bf
Sergey Lebedev <sergeylebedev87@gmail.com>
2022-04-25 21:01:11 +0300

    1 project

---
+++ 1 урок/1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int numResult = num * num;
+Console.Write($"Квадрат числа {num} равен ");
+Console.WriteLine(numResult);
+Console.ReadKey();
