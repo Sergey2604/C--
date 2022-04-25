8ef38ca44bf72f021ed12fd818e2287cdb103657
Sergey Lebedev <sergeylebedev87@gmail.com>
2022-04-25 21:47:24 +0300

    Задача 2

---
+++ 1 урок/2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число 1");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int num2 = int.Parse(Console.ReadLine());
+bool numResult = num2 * num2 == num1;
+Console.WriteLine(numResult ? "Yes" : "No");
+Console.ReadKey();
