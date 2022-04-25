e93af0bc9ef5f2f78d7ef1d5fa74a02e769c3701
Sergey Lebedev <sergeylebedev87@gmail.com>
2022-04-25 21:58:26 +0300

    3 задача

---
+++ 1 урок/3/Program.cs
@@ -0,0 +1,9 @@
+﻿string dayWeek = Console.ReadLine();
+if (dayWeek == "1") Console.WriteLine("Сегодня понедельник");
+else if (dayWeek == "2") Console.WriteLine("Сегодня втроник");
+else if (dayWeek == "3") Console.WriteLine("Сегодня среда");
+else if (dayWeek == "4") Console.WriteLine("Сегодня четверг");
+else if (dayWeek == "5") Console.WriteLine("Сегодня пятница");
+else if (dayWeek == "6") Console.WriteLine("Сегодня суббота");
+else if (dayWeek == "7") Console.WriteLine("Сегодня воскресенье");
+else Console.Write("Ошибка ввода");
