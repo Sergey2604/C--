8a1856d86732781ca095b7f620cefbf2c4d84c03
Sergey Lebedev <sergeylebedev87@gmail.com>
2022-05-12 20:36:02 +0300

    Task9

---
+++ 2 урок/Task 9/Program.cs
@@ -0,0 +1,11 @@
+﻿int num = new Random().Next(10, 100);
+System.Console.WriteLine(num);
+int firstDigit = num / 10;
+int secondDigit = num % 10;
+System.Console.WriteLine(firstDigit);
+System.Console.WriteLine(secondDigit);
+if (firstDigit > secondDigit)
+{
+    System.Console.WriteLine(firstDigit);
+}
+else System.Console.WriteLine(secondDigit);
